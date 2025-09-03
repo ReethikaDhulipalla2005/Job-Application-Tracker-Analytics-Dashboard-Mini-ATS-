c4ab03dcc0fffec44e4cc223f572395d83949b8b
ReethikaDhulipalla2005 <dhulipallareethika@gmail.com>
2025-09-04 01:05:53 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://job-application-tracker-analytics-d.vercel.app/',
         changeOrigin: true,
         secure: false,
       }
